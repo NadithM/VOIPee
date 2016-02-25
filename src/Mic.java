6e07523e58e0f40869ae1524a40fb8632990b5f9
RajithaRama <rajitharamanayake@gmail.com>
2016-02-25 22:36:39 +0530

    Mic over. Not tested.

---
+++ src/Mic.java
@@ -0,0 +1,119 @@
+import javax.sound.sampled.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by Rama on 2/25/2016.
+ */
+public class Mic implements Runnable {
+    int PORT;
+    DatagramSocket socket;
+    String HOST;
+    public boolean oncall = false;
+    boolean stopCapture = false;
+
+    ByteArrayOutputStream byteArrayOutputStream;
+    AudioFormat audioFormat;
+    TargetDataLine targetDataLine;
+    AudioInputStream audioInputStream;
+    SourceDataLine sourceDataLine;
+    byte [] tempBuffer = new byte[500];
+
+    public Mic(int port, DatagramSocket sock, String IP){
+        this.PORT = port;
+        this.socket = sock;
+        this.HOST = IP;
+    }
+
+    private AudioFormat getAudioFormat() {
+        float sampleRate = 16000.0F;
+        int sampleSizeInBits = 16;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+    }
+
+
+
+    private void captureAudio() {
+
+        try {
+            Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();    //get available mixers
+            System.out.println("Available mixers:");
+            Mixer mixer = null;
+            for (int cnt = 0; cnt < mixerInfo.length; cnt++) {
+                System.out.println(cnt + " " + mixerInfo[cnt].getName());
+                mixer = AudioSystem.getMixer(mixerInfo[cnt]);
+
+                Line.Info[] lineInfos = mixer.getTargetLineInfo();
+                if (lineInfos.length >= 1 && lineInfos[0].getLineClass().equals(TargetDataLine.class)) {
+                    System.out.println(cnt + " Mic is supported!");
+                    break;
+                }
+            }
+
+            audioFormat = getAudioFormat();     //get the audio format
+            DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+
+            if (mixer == null) throw new AssertionError();
+            targetDataLine = (TargetDataLine) mixer.getLine(dataLineInfo);
+            targetDataLine.open(audioFormat);
+            targetDataLine.start();
+
+            DataLine.Info dataLineInfo1 = new DataLine.Info(SourceDataLine.class, audioFormat);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo1);
+            sourceDataLine.open(audioFormat);
+            sourceDataLine.start();
+
+            //Setting the maximum volume
+            FloatControl control = (FloatControl)sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+            control.setValue(control.getMaximum());
+
+            captureAndPlay(); //playing the audio
+
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+    }
+
+    private void captureAndPlay() {
+
+        stopCapture = false;
+
+        int readCount;
+        while (!stopCapture) {
+            readCount = targetDataLine.read(tempBuffer, 0, tempBuffer.length);  //capture sound into tempBuffer
+            if (readCount > 0) {
+                send();
+            }
+        }
+
+    }
+
+    private void send() {
+        try {
+            InetAddress host = InetAddress.getByName( HOST ) ;
+            DatagramPacket packet = new DatagramPacket(tempBuffer, tempBuffer.length, host, PORT);
+            socket.send( packet ) ;
+        }
+        catch( Exception e ) {
+                System.out.println( e ) ;
+            e.printStackTrace();
+            }
+            // Send the packet
+
+    }
+
+    public void run(){
+        oncall = true;
+        while(oncall) {
+            captureAudio();
+        }
+    }
+}
--- src/VOIPee.java
+++ src/VOIPee.java
@@ -1,8 +1,21 @@
 /**
  * Created by Nadith on 2/24/2016.
  */
-public class VOIPee {
+public class VOIPee{
+
+    private static final int PORT = 20000;
+
+
+
     public static void main(String[] args) {
+
+
+        VOIPee voice = new VOIPee();
+
+    }
+
+
+    public VOIPee(){
 
     }
 
